0648536677ba0e3a101833b44d9fe1197497859d
dfcarpenter <dfcarpenter@gmail.com>
2021-08-15 14:25:54 -0700

    initial segment implementation

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -105,3 +105,47 @@ func (s *segment) Read(off uint64) (*api.Record, error) {
 	err = proto.Unmarshal(p, record)
 	return record, err
 }
+
+/*
+IsMaxed returns whether the segment has reached its max size
+If you wrote a small number of long logs then you'd hit the segment bytes limit; if you wrote a lot of small logs,
+then you'd hit the index bytes limit.
+*/
+func (s *segment) IsMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxStoreBytes ||
+		s.index.size >= s.config.Segment.MaxIndexBytes
+}
+
+func (s *segment) Remove() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
+	if err := os.Remove(s.index.Name()); err != nil {
+		return err
+	}
+	if err := os.Remove(s.store.Name()); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+/*
+nearestMultiple returns lesser multiple between two numbers to make sure we stay under
+users disk capacity.
+*/
+func nearestMultiple(j, k uint64) uint64 {
+	if j >= 0 {
+		return (j / k) * k
+	}
+	return ((j - k + 1) / k) * k
+}
