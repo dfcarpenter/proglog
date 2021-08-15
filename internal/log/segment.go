a850a5df039a4566eba662ea6a70d09383b648aa
dfcarpenter <dfcarpenter@gmail.com>
2021-08-15 14:01:13 -0700

    core log implementation

---
+++ internal/log/segment.go
@@ -0,0 +1,107 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	api "github.com/dfcarpenter/proglog/api/v1"
+	"google.golang.org/protobuf/proto"
+
+)
+
+
+/*
+The segment wraps the index and store types to coordinate operations across the two.
+*/
+type segment struct {
+	store *store
+	index *index
+	baseOffset, nextOffset uint64
+	config Config
+}
+
+/*
+newSegment is called when the log needs to add a new segment such as when the current active segment hits its max size.
+
+If the index is empty, then the next record appended to the segment would be the first record and its offset would
+be the segments base offset.
+*/
+func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
+	s := &segment{
+		baseOffset: baseOffset,
+		config: c,
+	}
+	var err error
+	storeFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".store")),
+		os.O_RDWR|os.O_CREATE|os.O_APPEND,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.store, err = newStore(storeFile); err != nil {
+		return nil, err
+	}
+	indexFile, err := os.OpenFile(
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".index")),
+		os.O_RDWR|os.O_CREATE,
+		0644,
+	)
+	if err != nil {
+		return nil, err
+	}
+	if s.index, err = newIndex(indexFile, c); err != nil {
+		return nil, err
+	}
+	if off, _, err := s.index.Read(-1); err != nil {
+		s.nextOffset = baseOffset
+	} else {
+		s.nextOffset = baseOffset + uint64(off) + 1
+	}
+	return s, nil
+
+}
+
+/*
+Append write the record to the segment and returns the cursor to the newly appended record's offset. The log returns
+the offset to the API response. The segment appends a record in a two step process: it appends the data to the store
+and then adds an index entry.
+*/
+func (s *segment) Append(record *api.Record) (offset uint64, err error) {
+	cursor := s.nextOffset
+	record.Offset = cursor
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+	_, pos, err := s.store.Append(p)
+	if err = s.index.Write(
+		// index offsets are relative to base offset
+		uint32(s.nextOffset-uint64(s.baseOffset)),
+		pos,
+	); err != nil {
+		return 0, err
+	}
+	s.nextOffset++
+	return cursor, nil
+
+}
+
+/*
+Read
+*/
+func (s *segment) Read(off uint64) (*api.Record, error) {
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+	record := &api.Record{}
+	err = proto.Unmarshal(p, record)
+	return record, err
+}
--- internal/log/store.go
+++ internal/log/store.go
@@ -3,4 +3,107 @@ package log
 import (
 	"bufio"
 	"encoding/binary"
+	"os"
+	"sync"
 )
+
+var (
+	// enc defines the encoding that we persist the record sizes and index entries in
+	enc = binary.BigEndian
+)
+
+const (
+	// number of bytes used to store the records length
+	lenWidth = 8
+)
+
+/*
+Simple wrapper around file with two APIs to append and read bytes to and from the file
+*/
+type store struct {
+	*os.File
+	mu sync.Mutex
+	buf *bufio.Writer
+	size uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	// Get file info especially size
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	// Get file size in uint64
+	size := uint64(fi.Size())
+	return &store{
+		File: f,
+		size: size,
+		buf: bufio.NewWriter(f),
+	}, nil
+}
+
+/*
+Append adds
+*/
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+	// Write to buffered writer instead of file directly to reduce the number of system calls and improve performance
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+/*
+Read returns the record stored at the given position
+*/
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	// First flush write buffer, in case we're about to try to read a record
+	// that the buffer hasn't flushed to disk yet.
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+/*
+ReadAt reads len(p) bytes into p beginning at the off offset in the store's file.
+*/
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	// defer causes mu.Unlock() to be executed when the current scope is executed ( e.g. a function that returns )
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	err := s.buf.Flush()
+	if err != nil {
+		return err
+	}
+	return s.File.Close()
+}
+
